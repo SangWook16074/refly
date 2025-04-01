be5d29e9e34ec676db9eb7f695061ec90bb3b8e1
sangwook16074 <qazws78941@gmail.com>
2025-04-02 01:55:41 +0900

    드래그 시트 기능 추가

--- lib/src/ui/main_ui.dart
+++ lib/src/ui/main_ui.dart
@@ -14,6 +14,7 @@ class MainUI extends StatelessWidget {
       children: [
         Container(
           height: size.height * 0.7,
+          alignment: Alignment.topCenter,
           decoration: BoxDecoration(
               gradient: RadialGradient(
                   focal: Alignment.bottomRight,
@@ -22,11 +23,8 @@ class MainUI extends StatelessWidget {
                 theme.colorScheme.secondary,
                 theme.colorScheme.primary,
               ])),
-        ),
-        Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Padding(
+          child: SafeArea(
+            child: Padding(
               padding: const EdgeInsets.all(16.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -43,52 +41,65 @@ class MainUI extends StatelessWidget {
                 ],
               ),
             ),
-            Container(
-              height: size.height * 0.8,
-              decoration: BoxDecoration(
-                  borderRadius:
-                      const BorderRadius.vertical(top: Radius.circular(16.0)),
-                  color: theme.scaffoldBackgroundColor),
-              child: DefaultTabController(
-                  length: 2,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: Column(
-                      children: [
-                        TabBar(
-                            unselectedLabelStyle: TextStyle(
-                              color: const Color(0xff000000).withOpacity(0.6),
-                              fontFamily: "Roboto",
-                              fontWeight: FontWeight.w600,
-                              fontSize: 15,
-                            ),
-                            labelStyle: TextStyle(
-                                color: theme.colorScheme.primary,
-                                fontFamily: "Roboto",
-                                fontWeight: FontWeight.w600,
-                                fontSize: 15),
-                            tabs: const [
-                              Tab(
-                                text: "오늘 기록",
-                              ),
-                              Tab(
-                                text: "미해결 문제",
-                              ),
-                            ]),
-                        Expanded(
-                          child: TabBarView(children: [
-                            const ProblemView(),
-                            Container(
-                              height: 20,
-                            ),
-                          ]),
-                        )
-                      ],
-                    ),
-                  )),
-            )
-          ],
-        )
+          ),
+        ),
+        DraggableScrollableSheet(
+            initialChildSize: 0.8,
+            maxChildSize: 0.8,
+            minChildSize: 0.4,
+            snap: true,
+            snapSizes: const [.7],
+            builder: (context, scrollController) {
+              return SingleChildScrollView(
+                controller: scrollController,
+                physics: const ClampingScrollPhysics(),
+                child: Container(
+                  height: size.height * 0.8,
+                  decoration: BoxDecoration(
+                      borderRadius: const BorderRadius.vertical(
+                          top: Radius.circular(16.0)),
+                      color: theme.scaffoldBackgroundColor),
+                  child: DefaultTabController(
+                      length: 2,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        child: Column(
+                          children: [
+                            TabBar(
+                                unselectedLabelStyle: TextStyle(
+                                  color:
+                                      const Color(0xff000000).withOpacity(0.6),
+                                  fontFamily: "Roboto",
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 15,
+                                ),
+                                labelStyle: TextStyle(
+                                    color: theme.colorScheme.primary,
+                                    fontFamily: "Roboto",
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: 15),
+                                tabs: const [
+                                  Tab(
+                                    text: "오늘 기록",
+                                  ),
+                                  Tab(
+                                    text: "미해결 문제",
+                                  ),
+                                ]),
+                            Expanded(
+                              child: TabBarView(children: [
+                                const ProblemView(),
+                                Container(
+                                  height: 20,
+                                ),
+                              ]),
+                            )
+                          ],
+                        ),
+                      )),
+                ),
+              );
+            })
       ],
     ));
   }
--- lib/src/ui/problem/view/empty_view.dart
+++ lib/src/ui/problem/view/empty_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:solution_diary_app/src/ui/widgets/upload_button.dart';
 
 class EmptyView extends StatelessWidget {
   const EmptyView({super.key});
@@ -6,9 +7,19 @@ class EmptyView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Center(
-        child: Text(
-      "해결과정을 기록하면 성장할 수 있어요!\n오늘 생긴 궁금증을 적어볼까요?",
-      textAlign: TextAlign.center,
-    ));
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            "해결과정을 기록하면 성장할 수 있어요!\n오늘 생긴 궁금증을 적어볼까요?",
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(
+            height: 50,
+          ),
+          UploadButton()
+        ],
+      ),
+    );
   }
 }
